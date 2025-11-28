bda9ac2cce5a8e558c320886bbb2db83d6a179d2
Pablo Carrillo Punina <pablocarrillopunina@gmail.com>
2025-11-28 09:17:17 +0100

    feat: configuracion inicial e integracion de JavaFX con spring

--- src/main/java/pablocarrillo/tareas/TareasApplication.java
+++ src/main/java/pablocarrillo/tareas/TareasApplication.java
@@ -1,13 +1,16 @@
 package pablocarrillo.tareas;
 
+import javafx.application.Application;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import pablocarrillo.tareas.presentacion.SistemasTareasFx;
 
 @SpringBootApplication
 public class TareasApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(TareasApplication.class, args);
+//		SpringApplication.run(TareasApplication.class, args);
+        Application.launch(SistemasTareasFx.class, args);
 	}
 
 }
