2c6093d6842966aa3a34345b30a16176cf32a9e0
Pablo Carrillo Punina <pablocarrillopunina@gmail.com>
2025-11-29 21:24:01 +0100

    feat:implementar tabla con JavaFX y listado de tareas con JavaFX y Spring

--- src/main/java/pablocarrillo/tareas/controlador/IndexControlador.java
+++ src/main/java/pablocarrillo/tareas/controlador/IndexControlador.java
@@ -1,15 +1,63 @@
 package pablocarrillo.tareas.controlador;
 
+import javafx.beans.Observable;
+import javafx.collections.FXCollections;
+import javafx.collections.ObservableList;
+import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
+import javafx.scene.control.SelectionMode;
+import javafx.scene.control.TableColumn;
+import javafx.scene.control.cell.PropertyValueFactory;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
 
 import java.net.URL;
 import java.util.ResourceBundle;
 
+import pablocarrillo.tareas.modelo.Tarea;
+import pablocarrillo.tareas.servicio.TareaServicio;
+import javafx.scene.control.TableView;   // ✔️ IMPORT CORRECTO
+
 @Component
 public class IndexControlador implements Initializable {
+
+    private static final Logger logger = LoggerFactory.getLogger(IndexControlador.class);
+
+    @Autowired
+    private TareaServicio tareaServicio;
+
+    @FXML
+    private TableView<Tarea> tareaTabla;   // ✔️ Ya no dará error
+
+    @FXML
+    private TableColumn<Tarea, Integer> idTareaColumna;
+
+    @FXML
+    private TableColumn<Tarea, String> nombreTareaColumna;
+
+    @FXML
+    private TableColumn<Tarea, String> responsableColumna;
+
+    @FXML
+    private TableColumn<Tarea, String> estatusColumna;
+
+    private final ObservableList<Tarea> tareaList =
+            FXCollections.observableArrayList();
+
     @Override
     public void initialize(URL url, ResourceBundle resourceBundle) {
+        tareaTabla.getSelectionModel().setSelectionMode(SelectionMode.SINGLE);
+        configurarColumnas();
 
+
+    }
+
+    private void configurarColumnas(){
+        idTareaColumna.setCellValueFactory(new PropertyValueFactory<>("idTarea"));
+        nombreTareaColumna.setCellValueFactory(new PropertyValueFactory<>("nombreTarea"));
+        responsableColumna.setCellValueFactory(new PropertyValueFactory<>("responsable"));
+        estatusColumna.setCellValueFactory(new PropertyValueFactory<>("estatus"));
     }
 }
